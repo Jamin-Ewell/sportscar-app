e3e3ec7f5e1ce2c4affd10902f55815704bbb928
Jamin Ewell <jamin@lunaserve.com>
2024-01-03 14:30:58 +0200

    add entities and some cleanup

---
+++ src/Domain/Entities/Dealer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sportscar_app.Domain.Entities;
+public class Dealer
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+    public bool IsAdmin { get; set; }
+}
--- src/Domain/Exceptions/UnsupportedColourException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace sportscar_app.Domain.Exceptions;
-
-public class UnsupportedColourException : Exception
-{
-    public UnsupportedColourException(string code)
-        : base($"Colour \"{code}\" is unsupported.")
-    {
-    }
-}
