0cc9941228f5759203be59e88e75d811f35cd542
Jamin Ewell <jamin@lunaserve.com>
2024-01-08 01:11:21 +0200

    infrastructure and entities

---
+++ Infrastructure/Persistence/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence
+{
+    public class Context : DbContext
+    {
+
+
+        public DbSet<Car>? Cars { get; set; }
+
+        public DbSet<User>? User { get; set; }
+
+        public DbSet<Dealer>? Dealer { get; set; }
+    }
+}
